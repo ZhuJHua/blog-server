4b5270873470ae32e018cb1f7a9a0ceb7b56a18e
住京华 <1624109111@qq.com>
2023-12-14 15:29:30 +0800

    Initial commit

---
+++ src/main/java/com/zjh/j2eework/J2EeWorkApplication.java
@@ -0,0 +1,13 @@
+package com.zjh.j2eework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J2EeWorkApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(J2EeWorkApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/com/zjh/j2eework/J2EeWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjh.j2eework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class J2EeWorkApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
