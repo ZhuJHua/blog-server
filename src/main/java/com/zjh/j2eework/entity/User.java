d04e029e0e38312dec65b262dbff23531560048a
住京华 <1624109111@qq.com>
2023-12-23 10:58:00 +0800

    fix:添加User类权限字段

--- src/main/java/com/zjh/j2eework/entity/User.java
+++ src/main/java/com/zjh/j2eework/entity/User.java
@@ -31,6 +31,9 @@ public class User {
     @Column(name = "password")
     private String password;
     
+    @Column(name = "auth")
+    private Integer auth;
+    
     @ToString.Exclude
     @OneToMany(mappedBy = "user", orphanRemoval = true)
     @OrderBy("id")
